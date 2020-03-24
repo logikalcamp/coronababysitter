10a9f890eb5195b0391bce539a6127a3ade2f910
= <abenjaminov@gmail.com>
2020-03-24 18:15:08 +0200

    fix close mongo client after request ends

--- server/controllers/Doctor.js
+++ server/controllers/Doctor.js
@@ -6,39 +6,39 @@ var Doctor = require('../service/DoctorService');
 module.exports.createDoctor = function createDoctor (req, res, next, body, docId) {
   Doctor.createDoctor(body, docId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     });
 };
 
 module.exports.getAllDoctors = function getAllDoctors (req, res, next) {
   Doctor.getAllDoctors()
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     });
 };
 
 module.exports.getDoctorById = function getDoctorById (req, res, next, docId) {
   Doctor.getDoctorById(docId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     });
 };
 
 module.exports.registerDoctor = function registerDoctor (req, res, next, body) {
   Doctor.registerDoctor(body)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     });
 };
--- server/controllers/Session.js
+++ server/controllers/Session.js
@@ -6,29 +6,29 @@ var Session = require('../service/SessionService');
 module.exports.createSession = function createSession (req, res, next, body) {
   req.SessionService.createSession(body)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     });
 };
 
 module.exports.getAllSessionsByUser = function getAllSessionsByUser (req, res, next, body, userId) {
   req.SessionService.getAllSessionsByUser(body, userId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     });
 };
 
 module.exports.updateSession = function updateSession (req, res, next, body, sessionId) {
   req.SessionService.updateSession(body, sessionId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(req,res, response);
     });
 };
