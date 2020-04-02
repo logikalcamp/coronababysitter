6c27f7c4908f969fcfe2990e3f252cf583fc2a56
Yoav Shotland <shotland16@gmail.com>
2020-04-02 22:35:10 +0300

    distance function tested

--- server/service/SessionService.js
+++ server/service/SessionService.js
@@ -114,18 +114,20 @@ class SessionService {
         MongoDB.findManyAggregate(COLLECTION_NAME, {aggregate: aggregate}, this.MongoClient).then((result2) => {
           sessions = result2;
           var available = [];
-        /*  sessions.forEach((element) => {
+          console.log(sessions);
+          sessions.forEach((element) => {
             if(element){
-              console.log(element);
-              console.log(user.lat + "   "+ user.lon + "    " + element.doctor_o.lat +"   " + element.doctor_o.long);
-              if(Location.getDistance(user.lat, user.lon, (element.doctor_o).pop.lat, (element.doctor_o).pop.lon) < X){
+              console.log(Location.getDistance(user.lat, user.lon, element.doctor_o[0].lat, element.doctor_o[0].lon));
+
+              console.log(user.lat + "   "+ user.lon + "    " + element.doctor_o[0].lat +"   " + element.doctor_o[0].lon);
+              if(Location.getDistance(user.lat, user.lon, element.doctor_o[0].lat, element.doctor_o[0].lon) < X){
                 available.push(element);
               }
             }
           
           });
-         */
-         resolve(sessions);
+         
+         resolve(available);
         });
     });
   });
