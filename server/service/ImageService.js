d3cf52bae1caf002476a6e6c0fb6013b3c2c1a26
= <abenjaminov@gmail.com>
2020-04-05 21:03:09 +0300

    images

--- server/service/ImageService.js
+++ server/service/ImageService.js
@@ -1,42 +1,55 @@
-const {GPhotos} = require('upload-gphotos');
-const fs = require('fs');
-var path = require('path');
+//const {GPhotos} = require('upload-gphotos');
+//const fs = require('fs');
+//var path = require('path');
+var cloudinary = require('cloudinary').v2;
 
+cloudinary.config({
+    cloud_name: 'sitterseekercloud',
+    api_key: '128644798248273',
+    api_secret: 'ElA61Avn46r7AKPMl4V7b7all_8'
+});
 
 class ImageService {
     constructor() {
 
     }
 
-    async uploadImageToGooglePhotos(albumName, filePath) {
-        const gphotos = new GPhotos();
-    
-        var username = 'appsitterseeker@gmail.com';
-        var password = 'sitterseeker2020';
+    uploadImageToGooglePhotos(albumName, filePath) {
+        return new Promise((resolve,reject) => {
+            cloudinary.uploader.upload(filePath, {resource_type:"auto"}, (error,result) => {
+                if(error) reject(error)
 
-        try {
+                resolve({rawUrl: result.url, id:result.public_id});
+            });
+        });
+        // const gphotos = new GPhotos();
+    
+        // var username = 'appsitterseeker@gmail.com';
+        // var password = 'sitterseeker2020';
+
+        // try {
 
         
-            await gphotos.signin({
-                username,
-                password,
-            });
+        //     await gphotos.signin({
+        //         username,
+        //         password,
+        //     });
             
-            const album = await gphotos.searchAlbum({ title: albumName });
+        //     const album = await gphotos.searchAlbum({ title: albumName });
 
-            const photo = await gphotos.upload({
-                stream: fs.createReadStream(filePath),
-                size: (await fs.promises.stat(filePath)).size,
-                filename: path.basename(filePath),
-            });
+        //     const photo = await gphotos.upload({
+        //         stream: fs.createReadStream(filePath),
+        //         size: (await fs.promises.stat(filePath)).size,
+        //         filename: path.basename(filePath),
+        //     });
 
-            await album.append(photo);
+        //     await album.append(photo);
 
-            return photo;
-        }
-        catch (error)  {
-            return undefined;
-        }
+        //     return photo;
+        // }
+        // catch (error)  {
+        //     return undefined;
+        // }
     }
 }
 
