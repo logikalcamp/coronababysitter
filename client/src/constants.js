e91ea149427b03d0ec91e7a816724c82925483aa
= <abenjaminov@gmail.com>
2020-04-07 16:54:45 +0300

    consts

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,4 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 export const BASE_URL = isLocal ? "http://localhost:3002" : '';
+export const CNC_CONNECTED = "cnc-connected";
--- client/src/private-route/PrivateRoute.js
+++ client/src/private-route/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import {BASE_URL} from '../constants';
+import {CNC_CONNECTED} from '../constants';
 import axios from "axios";
 import {loginUser} from '../actions/auth'
 
@@ -13,13 +13,13 @@ const db = () => {
 
 const PrivateRoute = ({ component: Component, auth,dispatch, ...rest }) => {
   var ping = '';
-  var cncConnected = JSON.parse(localStorage.getItem('cnc-connected'));
+  var cncConnected = JSON.parse(localStorage.getItem(CNC_CONNECTED));
 
   if(rest.path.startsWith('/cnc') && !cncConnected) {
     var ping = prompt("על מנת להמשיך הזן ססמא");
 
     if(ping == 'חיבוק')
-      localStorage.setItem("cnc-connected",true)
+      localStorage.setItem(CNC_CONNECTED,true)
   }
 
   return (
