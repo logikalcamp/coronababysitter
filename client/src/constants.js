507974ed84cbd373df30557d36c6567db47c37df
= <abenjaminov@gmail.com>
2020-04-07 17:17:19 +0300

    3001

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-export const BASE_URL = isLocal ? "http://localhost:3002" : '';
+export const BASE_URL = isLocal ? "http://localhost:3001" : '';
 export const CNC_CONNECTED = "cnc-connected";
